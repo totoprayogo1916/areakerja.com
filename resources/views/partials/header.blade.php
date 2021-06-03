e47b8ae1d2156e715da9893e84727e168a2e4d25
5h1ro <nurhakikiajha@gmail.com>
2021-06-03 09:24:41 +0700

    add

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,19 @@
 <header id="header" id="home">
     <div class="container">
+
         <div class="row align-items-center justify-content-between d-flex">
-          <div id="logo">
-            <a href="{{ route('home') }}"><img src="{{ asset('img/logo2.png') }}" alt="" title="" /></a>
-          </div>
+
+            <div id="logo">
+              <a href="{{ route('home') }}"><img src="{{ asset('img/logo2.png') }}" alt="" title="" /></a>
+            </div>
+
+
+            <div class="col-lg-2 form-cols">
+                <button type="submit" class="btn btn-area">
+                  <span class="lnr lnr-file-add"></span> Pasang Lowongan
+                </button>
+            </div>
         </div>
+
     </div>
 </header>
