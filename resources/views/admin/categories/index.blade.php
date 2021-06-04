00cad5032686e5446a7379b544b7834e23a1dfd3
5h1ro <nurhakikiajha@gmail.com>
2021-06-04 16:27:42 +0700

    category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.admin')
 @section('content')
-@can('category_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.categories.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.category.title_singular') }}
-            </a>
-        </div>
+<div style="margin-bottom: 10px;" class="row">
+    <div class="col-lg-12">
+        <a class="btn btn-success" href="{{ route("admin.categories.create") }}">
+            {{ trans('global.add') }} {{ trans('cruds.category.title_singular') }}
+        </a>
     </div>
-@endcan
+</div>
 <div class="card">
     <div class="card-header">
         {{ trans('cruds.category.title_singular') }} {{ trans('global.list') }}
@@ -46,25 +44,20 @@
                                 {{ $category->name ?? '' }}
                             </td>
                             <td>
-                                @can('category_show')
-                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.categories.show', $category->id) }}">
-                                        {{ trans('global.view') }}
-                                    </a>
-                                @endcan
 
-                                @can('category_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.categories.edit', $category->id) }}">
-                                        {{ trans('global.edit') }}
-                                    </a>
-                                @endcan
+                                <a style="width: 60px" class="btn btn-xs btn-primary" href="{{ route('admin.categories.show', $category->id) }}">
+                                    {{ trans('global.view') }}
+                                </a>
 
-                                @can('category_delete')
-                                    <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                    </form>
-                                @endcan
+                                <a style="width: 60px" class="btn btn-xs btn-info" href="{{ route('admin.categories.edit', $category->id) }}">
+                                    {{ trans('global.edit') }}
+                                </a>
+
+                                <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}" style="width: 60px">
+                                </form>
 
                             </td>
 
