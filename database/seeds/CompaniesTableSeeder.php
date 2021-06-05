1eeaeb4eb83e4b14be7acd1481ca4c36f0f9eb74
fadelmi12 <fadelirsyad04@gmail.com>
2021-06-05 11:16:54 +0700

    gambar

--- database/seeds/CompaniesTableSeeder.php
+++ database/seeds/CompaniesTableSeeder.php
@@ -14,36 +14,43 @@ class CompaniesTableSeeder extends Seeder
     {
         $company =  Company::create([
             'name' => 'Areakerja',
+            'gambar' => 'Areakerja.jpeg'
         ]);
         $company->save();
 
         $company =  Company::create([
             'name' => 'Seven Inc',
+            'gambar' => 'Seven Inc.png'
         ]);
         $company->save();
 
         $company =  Company::create([
             'name' => 'KopiSoe',
+            'gambar' => 'kopisoe.png'
         ]);
         $company->save();
 
         $company =  Company::create([
             'name' => 'Jogja Tailor',
+            'gambar' => 'Jogja Tailor.png'
         ]);
         $company->save();
 
         $company =  Company::create([
             'name' => 'The Oxalis Regency Hotel',
+            'gambar' => 'The Oxalis Regency Hotel.png'
         ]);
         $company->save();
 
         $company =  Company::create([
             'name' => 'JW Media Production',
+            'gambar' => 'JW Media Production.jpg'
         ]);
         $company->save();
 
         $company =  Company::create([
             'name' => 'Dynamic Space',
+            'gambar' => 'Dynamic Space.png'
         ]);
         $company->save();
 
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -5,27 +5,28 @@
 @section('content')
 <div class="col-lg-8 post-list">
     @foreach($jobs as $job)
-        <div data-aos="fade-up" class="single-post d-flex flex-row">
-            <div class="thumb">
-                @if($job->company->logo)
-                    <img src="{{ $job->company->logo->getUrl() }}" alt="">
-                @endif
-            </div>
-            <div class="details">
-                <div class="title d-flex flex-row justify-content-between">
-                    <div class="titles">
-                        <a href="{{ route('jobs.show', $job->id) }}"><h4>{{ $job->title }}</h4></a>
-                        <h6><span class="lnr lnr-apartment"></span> {{ $job->company->name }}</h6>
-                    </div>
+        <a href="{{ route('jobs.show', $job->id) }}">
+            <div data-aos="fade-up" class="single-post align-items-center d-flex flex-row">
+                <div class="thumb">
+                    @if($job->company->gambar)
+                        <img src="{{ url('img/companylogo') }}/{{ $job->company->gambar }}" style="width: 250px; height: 200px;">
+                    @endif
                 </div>
-                <p>
-                    {{ $job->short_description }}
-                </p>
-                <h5>Job Nature: {{ $job->job_nature }}</h5>
-                <p class="address"><span class="lnr lnr-map"></span> {{ $job->address }}</p>
-                <p class="address"><span class="lnr lnr-database"></span> {{ $job->salary }}</p>
+                <div class="details">
+                    <div class="title d-flex flex-row justify-content-between">
+                        <div class="titles">
+                            <h4>{{ $job->title }}</h4>
+                            <h6 style="margin-top: 12px;" ><span class="lnr lnr-apartment"></span>  {{ $job->company->name }}</h6>	                    </div>
+                    </div>
+                    <h6 style="margin-top: 0px; text-align: justify">
+                        {{ $job->short_description }}
+                    </h6>
+                    <h6>Job Nature: {{ $job->job_nature }}</h6>
+                    <p class="address"><span class="lnr lnr-map"></span> {{ $job->address }}</p>
+                    <p class="address" style="margin-bottom: 0px;" ><span class="lnr lnr-database"></span> {{ $job->salary }}</p>
+                </div>
             </div>
-        </div>
+        </a>
     @endforeach
 </div>
 @endsection
