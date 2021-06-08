e7c0327b5ce3f7741f22daa200341c99c09f8d45
user <ti.ervany@gmail.com>
2021-06-08 15:47:17 +0700

    fadelcomedi

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,7 +4,7 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
         autoplay: true,
         autoplaySpeed: 3000
