0cc96c5cff4619b17852d86dc242678010ec4df6
user <ti.ervany@gmail.com>
2021-06-08 17:08:49 +0700

    kanan kiri

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,11 +1,10 @@
 $(function () {
     $('.multiple-items').slick({
-        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        prevArrow:"<button type='button' class='slick-prev '><img src='{{ asset('img/kiri.png') }}' aria-hidden='true'></button>",
+        nextArrow:"<button type='button' class='slick-next '><img src='{{ asset('img/kanan.png') }}' aria-hidden='true'></button>",
         autoplay: true,
         autoplaySpeed: 3000
     });
