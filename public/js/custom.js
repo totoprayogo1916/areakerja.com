4867936e6cdb8195f203da40b8b696df6213dadb
user <ti.ervany@gmail.com>
2021-06-09 09:26:43 +0700

    btn slickkkk

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,9 +3,11 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: true,
-        prevArrow:"<button type='button' class='slick-prev '><img src='{{ asset('img/kiri.png') }}' aria-hidden='true'></button>",
-        nextArrow:"<button type='button' class='slick-next '><img src='{{ asset('img/kanan.png') }}' aria-hidden='true'></button>",
+        prevArrow:"<button type='button' class='slick-prev prev' aria-hidden='true'></button>",
+        nextArrow:"<button type='button' class='slick-next next' aria-hidden='true'></button>",
         autoplay: true,
         autoplaySpeed: 3000
+
+       
     });
 });
