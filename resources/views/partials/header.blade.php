04490d99afb98a6b509754a08e297f778db0c3f7
ipaaaa <ti.rifanurh@gmail.com>
2021-06-09 13:04:44 +0700

    tips,kota

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,23 @@
 <header id="header" id="home">
-    <div class="container">
+  <div class="container">
+    <div class="row align-items-center justify-content-between d-flex">
+      
+      <div id="logo2" class="col-7 form-pasang2">
+        <a href="{{ route('home') }}"><img src="{{ asset('img/logo3.svg') }}" alt="" title="" /></a>
+      </div>
 
-        <div class="row align-items-center justify-content-between d-flex">
+      <a class="text-orange" href="#">
+        <span class="fa fa-newspaper-o mr-1"></span> Tips Kerja
+      </a>
 
-            <div id="logo2" class="col form-pasang2">
-              <a href="{{ route('home') }}"><img src="{{ asset('img/logo3.svg') }}" alt="" title="" /></a>
-            </div>
-
-
-
-            <a type="button" class="btn form-pasang" href="{{ route('login') }}">
-              <span class="lnr lnr-file-add"></span> Pasang Lowongan
-            </a>
-
-        </div>
+      <a class="text-orange" href="#" >
+        <span class="fa fa-map-marker mr-1"></span> Kota Lainnya
+      </a>
+      
+      <a type="button" class="btn form-pasang" href="{{ route('login') }}">
+        <span class="lnr lnr-file-add"></span> Pasang Lowongan
+      </a>
 
     </div>
+  </div>
 </header>
