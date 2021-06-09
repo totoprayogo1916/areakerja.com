3449f5c4963b714fdcd553cf6eac57dc7102b147
ipaaaa <ti.rifanurh@gmail.com>
2021-06-09 13:39:23 +0700

    icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,6 +9,7 @@
 <head>
 	<title>Login</title>
 	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="img/img-01.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,6 +10,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="img/img-01.png">
 
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://unpkg.com/@coreui/coreui@2.1.16/dist/css/coreui.min.css" rel="stylesheet" />
