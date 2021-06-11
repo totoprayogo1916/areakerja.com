3a4a8e7b4ce0e9b05b7bae02564c66a14c95e7a6
5h1ro <nurhakikiajha@gmail.com>
2021-06-11 13:04:06 +0700

    aboutus

--- resources/views/user/aboutus.blade.php
+++ resources/views/user/aboutus.blade.php
@@ -11,14 +11,14 @@
                         <h1>Tentang Kami</h1>
                         <p style="font-size: 11pt; font-style: bold; margin: 5%; text-align: left; line-height: 120%">
                             Areakerja adalah perusahaan berbasis teknologi informasi yang berpusat di Yogyakarta. Perusahaan ini berfokus pada platform untuk mencari lowongan kerja di Daerah Istimewa Yogyakarta (DIY).
-                            <br>
-                            <br>
+                        </p>
+                        <p style="font-size: 11pt; font-style: bold; margin: 5%; text-align: left; line-height: 120%">
                             DIY sebagai salah satu wilayah prestisius di Indonesia yang menyandang sebagai kota budaya dan pelajar membuat arus ekonomi dan bisnis di wilayah ini cukup besar. Banyak sekali perusahaan besar, menengah, dan kecil yang berdiri di wilayah DIY dan terus mengembangkan bisnisnya. Hal ini menuntut banyak dibukanya lowongan pekerjaan di DIY.
-                            <br>
-                            <br>
+                        </p>
+                        <p style="font-size: 11pt; font-style: bold; margin: 5%; text-align: left; line-height: 120%">
                             Banyaknya arus lowongan pekerjaan di Yogyakarta baik dari perusahaan besar, menengah, dan kecil menuntut diperlukannya sebuah media yang berfokus untuk menampung informasi lowongan pekerjaan yang ada di Yogyakarta.
-                            <br>
-                            <br>
+                        </p>
+                        <p style="font-size: 11pt; font-style: bold; margin: 5%; text-align: left; line-height: 120%; margin-bottom: -0.1%">
                             Untuk itu Areakerja hadir sebagai platform untuk membantu perusahaan mendapatkan kandidat terbaiknya, serta membantu para pencari kerja untuk menemukan pekerjaan yang diinginkan di DIY.
                         </p>
                     </div>
