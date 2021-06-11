38fc06d8db8256be3b653eb7a4cd067ee5d68430
fadelmi12 <fadelirsyad04@gmail.com>
2021-06-11 14:46:58 +0700

    show

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,6 +1,57 @@
 @extends('layouts.main')
 
-@section('banner', 'Job: '.$job->title)
+@section('home')
+<section class="relative" id="home">
+    <div class="banner-area">
+        <div class="overlay overlay-bg container" >
+            <h4 style="color: #000000; margin-top: 2.5% ">
+                Areakerja.com > {{ $job->address }} > Lowongan {{ $job->title }} di {{ $job->company->name }}
+                </h4>
+        </div>
+        <div class="row align-items-center justify-content-center" style="margin-right: 15px; margin-left: 15px;">
+            <div class="banner-content col-lg-12">
+                </h6>
+                 <form action="#" class="serach-form-area flex-wrap" style="width: 100%; margin-top: 5%">
+                    <div class="col form-wrap-main" id="form-luar">
+                        <div class="row form-wrap justify-content-center" id="form-dalam">
+                            <div class="col-lg-3 form-cols">
+                                <input type="text" class="form-control" placeholder="Cari Disini..">
+                            </div>
+                            <div class="col-lg-3 form-cols">
+                                <div class="default-select" id="default-selects">
+                                    <select>
+                                        <option value="0">All Areas</option>
+                                        @foreach($searchLocations as $id=>$searchLocations)
+                                            <option value="{{ $id }}">{{ $searchLocations }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 form-cols">
+                                <div class="default-select" id="default-selects2">
+                                    <select>
+                                        <option value="0">All Categories</option>
+                                        @foreach($searchCategories as $id=>$searchCategories)
+                                            <option value="{{ $id }}">{{ $searchCategories }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row form-wrap justify-content-around" style="margin-top: 2%">
+                            <div class="col-lg-3 form-cols">
+                                <button type="submit" class="btn btn-area ">
+                                <span class="lnr lnr-magnifier"></span> Search
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
 
 @section('content')
 <div class="col-lg-8 post-list" id="contentshow">
@@ -85,15 +136,14 @@
             </div>
             <hr class="d-flex flex-row" style="width: 100%; margin-left: 0%;"/>
             <div class="row" style="margin-top: 15px">
-                <div class="col-2" style="margin-right: 20%;">
+                <div class="col-2" style="margin-right: 10%;">
                     <a type="button" class="btn form-pasang3" href="{{ route('login') }}">
                         <h4 style="font-weight: normal"><span class="fa fa-paper-plane-o"></span><span style="display:inline-block; width: 10px;"></span>Lamar</h4>
                     </a>
                 </div>
-                <div class="col-2" style="margin-right: 20%;">
+                <div class="col-2" style="margin-right: 10%;">
                     <a type="button" class="btn form-pasang3" href="{{ route('login') }}">
                         <h4 style="font-weight: normal"><span class="fa fa-star-o"></span><span style="display:inline-block; width: 10px;"></span>Simpan</h4>
-
                     </a>
                 </div>
                 <div class="col-2">
