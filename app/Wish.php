98d1f0c8f6de753321c8895136fb2be93be382b8
fadelmi12 <fadelirsyad04@gmail.com>
2021-06-17 10:53:37 +0700

    d

---
+++ app/Wish.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wish extends Model
+{
+    use HasFactory;
+
+
+    public $table = 'wish';
+
+    protected $fillable = [
+        'ip'
+    ];
+}
