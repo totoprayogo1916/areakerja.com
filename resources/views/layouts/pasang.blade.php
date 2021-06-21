6bf2bf3d61e80fd1fc5c27e349d17c6d3c416941
user <ti.ervany@gmail.com>
2021-06-21 12:15:44 +0700

    layout artikel

--- resources/views/layouts/pasang.blade.php
+++ resources/views/layouts/pasang.blade.php
@@ -31,14 +31,17 @@
         <!-- Start post Area -->
         <section class="section-gap2">
             @yield('home')
+
         </section>
         <!-- End post Area -->
-
-        @include('partials.footer')
-
+        <div class="row justify-content-center d-flex mt-4"  >
+        @yield('content')
         </div>
 
 
+        </div>
+
+        @include('partials.footer')
         <script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
