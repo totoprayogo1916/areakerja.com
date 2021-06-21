a5d384d4804baccb0fcb1a465ce3a5a33a00c0a3
5h1ro <rajamodus7073@gmail.com>
2021-06-21 15:59:11 +0700

    a

--- database/seeds/ArtikelSeeder.php
+++ database/seeds/ArtikelSeeder.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Seeder;
 
 class ArtikelSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         $artikel = Artikel::create([
