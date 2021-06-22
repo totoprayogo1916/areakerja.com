92695e3a32d07db1613af5a4fc8359ad5f4a9f6a
ipaaaa <ti.rifanurh@gmail.com>
2021-06-22 13:13:06 +0700

    artikel

--- database/seeds/ArticleSeeder.php
+++ database/seeds/ArticleSeeder.php
@@ -16,7 +16,8 @@ class ArticleSeeder extends Seeder
     {
         $article = Article::create([
             'judul' => 'Cari Kerja Terasa Sulit? Mungkin, Kamu Perlu Tips Ini',
-            'ringkasan' => 'Belum punya pengalaman kerja, tentu akan menyulitkan seseorang untuk mendapatkan pekerjaan. Kamu bisa lakukan tips ini agar lebih berpotensi diterima kerja.',
+            'ringkasan' =>
+                'Belum punya pengalaman kerja, tentu akan menyulitkan seseorang untuk mendapatkan pekerjaan. Kamu bisa lakukan tips ini agar lebih berpotensi diterima kerja.',
             'content' => '<h1><strong>Tips&nbsp;Cari&nbsp;Kerja&nbsp;</strong></h1>
 
             <p>Sudah&nbsp;bukan&nbsp;lagi&nbsp;hal&nbsp;yang&nbsp;aneh&nbsp;bila&nbsp;mencari&nbsp;pekerjaan&nbsp;memang&nbsp;menjadi&nbsp;sebuah&nbsp;tantangan&nbsp;tersendiri&nbsp;bagi&nbsp;para&nbsp;pencarinya.Penolakan&nbsp;kerja&nbsp;berulang-ulang&nbsp;tentunya&nbsp;menjadi&nbsp;sebuah&nbsp;beban&nbsp;bagi&nbsp;para&nbsp;pencari&nbsp;kerja.&nbsp;Khususnya&nbsp;fresh&nbsp;graduate&nbsp;yang&nbsp;masih&nbsp;minim&nbsp;pengalaman&nbsp;kerja.Kondisi&nbsp;seperti&nbsp;ini&nbsp;sudah&nbsp;jelas&nbsp;akan&nbsp;membuat&nbsp;cari&nbsp;kerja&nbsp;terasa&nbsp;sulit&nbsp;dan&nbsp;terbebani.&nbsp;Apalagi,&nbsp;jika&nbsp;sudah&nbsp;berbulan-bulan&nbsp;tak&nbsp;dapat&nbsp;pekerjaan. Namun,&nbsp;semua&nbsp;itu&nbsp;bukan&nbsp;menjadi&nbsp;alasan.&nbsp;Karena,&nbsp;ada&nbsp;beberapa&nbsp;upaya&nbsp;atau&nbsp;tips&nbsp;yang&nbsp;bisa&nbsp;kamu&nbsp;lakukan&nbsp;untuk&nbsp;mempermudah&nbsp;kamu&nbsp;mencari&nbsp;kerja. Dimana,&nbsp;tips&nbsp;ini&nbsp;cukup&nbsp;efektif&nbsp;dalam&nbsp;mendapatkan&nbsp;lowongan&nbsp;kerja&nbsp;secara&nbsp;mudah,&nbsp;sesuai&nbsp;bidangnya,&nbsp;dan&nbsp;mudah&nbsp;dalam&nbsp;mengajukan&nbsp;pelamaran.&nbsp;Untuk&nbsp;lebih&nbsp;jelasnya,&nbsp;mari&nbsp;kita&nbsp;simak&nbsp;bersama&nbsp;bagaimana&nbsp;tips&nbsp;yang&nbsp;tepat&nbsp;untuk&nbsp;mencari&nbsp;kerja&nbsp;yang&nbsp;terasa&nbsp;sulit&nbsp;dan&nbsp;susah.</p>
@@ -28,7 +29,98 @@ class ArticleSeeder extends Seeder
             <p>Pertama,&nbsp;sudah&nbsp;jelas&nbsp;buat&nbsp;kamu&nbsp;yang&nbsp;kesulitan&nbsp;cari&nbsp;kerja,&nbsp;kamu&nbsp;bisa&nbsp;coba&nbsp;untuk&nbsp;mencari&nbsp;lowongan&nbsp;kerja&nbsp;di&nbsp;situs&nbsp;resmi&nbsp;perusahaan&nbsp;pengiklan&nbsp;loker.&nbsp;Cara&nbsp;ini&nbsp;jauh&nbsp;lebih&nbsp;efektif&nbsp;dan&nbsp;tak&nbsp;akan&nbsp;membuang buang&nbsp;waktumu&nbsp;dalam&nbsp;mencari&nbsp;loker.&nbsp;Sehingga,&nbsp;kamu&nbsp;bisa&nbsp;memanfaatkan&nbsp;waktu&nbsp;lebih&nbsp;efisien.&nbsp;Namun,&nbsp;sebaiknya&nbsp;gunakan&nbsp;situs&nbsp;penyedia&nbsp;iklan&nbsp;loker&nbsp;resmi&nbsp;yang&nbsp;sudah&nbsp;memiliki&nbsp;kredibilitas&nbsp;tinggi&nbsp;sebagai&nbsp;penyedia&nbsp;info&nbsp;lowongan&nbsp;kerja.&nbsp;Langkah&nbsp;ini&nbsp;untuk&nbsp;mencegah&nbsp;atau&nbsp;menghindari&nbsp;modus&nbsp;penipuan&nbsp;yang&nbsp;berkedok&nbsp;lowongan&nbsp;pekerjaan.&nbsp;Dengan&nbsp;begitu&nbsp;maka&nbsp;kamu&nbsp;akan&nbsp;lebih&nbsp;mudah&nbsp;menyaring&nbsp;mana&nbsp;lowongan&nbsp;yang&nbsp;benar-benar&nbsp;dan&nbsp;mana&nbsp;info&nbsp;loker&nbsp;menyesatkan.</p>
             
             <p>&nbsp;</p>',
-            'gambar' => 'Areakerja.jpeg'
+            'gambar' => 'Areakerja.jpeg',
+        ]);
+        $article->save();
+
+        $article = Article::create([
+            'judul' =>
+                'Tips Mengasah Komunikasi agar Bisa Hadapi Wawancara Kerja',
+            'ringkasan' =>
+                'Cara jitu yang perlu kamu pelajari untuk pertajam komunikasimu agar siap ketika wawancara kerja',
+            'content' => '<p><a href="http://www.lokerjogja.id/tips-mengasah-komunikasi-agar-bisa-hadapi-wawancara-kerja/">Tips Asah Komunikasi</a>&nbsp;&ndash; Biasanya, para pelamar kerja akan mengalami kegagalan atau tidak diterima kerja ketika memasuki tahap wawancara kerja.</p>
+
+            <p>Wajar saja mengingat banyak sekali yang terjebak oleh beberapa pertanyaan dari pewawancara yang tentu dijawab dengan jawaban yang kurang sesuai dengan kriteria perusahaan.</p>
+            
+            <p>Tak hanya itu, kendala komunikasi verbal pun juga turut berpengaruh kepada kualitas si pelamar kerja ketika sedang melalui seleksi wawancara.</p>
+            
+            <p>Bukan tanpa alasan, meski sudah dipersiapkan matang-matang materi apa yang hendak dihadapi saat wawancara namun bila komunikasinya kurang pas dan sesuai sangat dimungkinkan sekali akan membuat kamu gagal.</p>
+            
+            <p>Nah, oleh sebab itu, ada beberapa trik yang harus kamu coba untuk pertajam cara komunikasi kamu saat menghadapi tes wawancara kerja.</p>
+            
+            <p>Dimana, tips ini perlu dilatih secara kontinyu atau berkesinambungan selagi kamu mendapat panggilan wawancara kerja. Untuk lebih lengkapnya, yuk simak bersama tips jitunya.</p>
+            
+            <h2>Tips Kembangkan Komunikasi untuk Wawancara Kerja</h2>
+            
+            <p>Dalam prakteknya memang ada beberapa langkah atau tahapan tertentu yang perlu kamu persiapkan untuk menjawab segala pertanyaan yang terlontar oleh si pewawancara ketika memasuki seleksi wawancara kerja. Apa saja itu? Berikut ulasan lengkapnya.</p>
+            
+            <p><strong>1 . Latih Berbicara dengan Cermin</strong><br />
+            <img alt="" src="https://awsimages.detik.net.id/community/media/visual/2018/07/02/fc93ed59-39c2-4879-aa39-b31111d68d84_169.jpeg?w=700&amp;q=90" style="height:253px; width:450px" /></p>
+            
+            <p>Pertama, untuk mempermudah kamu dalam melatih skill komunikasi kamu, hendaknya lakukan latihan berbicara di depan cermin.</p>
+            
+            <p>Lihat dan cermati bagaimana kamu berbicara. Jika kamu merasa kurang percaya diri maka tingkatkan rasa percaya diri.</p>
+            
+            <p>Caranya, dengan menunjukkan nada bicara tegas namun lembut. Sehingga, akan terasa enak didengar. Jangan sepelekan, hal ini bisa mempengaruhi pewawancara untuk mempertimbangkan menerima kamu masuk kerja.</p>
+            
+            <p>Selain itu, coba tes suara dan nada bicara kamu sambil sedikit tersenyum. Sikap seperti ini menunjukkan bahwa kamu pribadi yang tegas namun kalem dan selalu optimis. Hal itu yang akan dilihat oleh si pewawancara yang bisa menarik mereka untuk merasa penasaran akan kemampuan kamu.</p>
+            
+            <p><strong>2. Bicara dengan Kecepatan Sedang</strong></p>
+            
+            <p>Selain itu, latihan teknis dalam berkomunikasi agar memberikan kesan menari bagi pewawancara kerja adalah dengan mencoba untuk berbicara dengan kecepatan sedang.</p>
+            
+            <p>Perlu untuk kamu ketahui bahwa berbicara dengan nada cepat menunjukkan bahwa kamu sedang cemas. Dan perlu diingat bahwa pewawancara sudah barang tentu tahu akan hal ini.</p>
+            
+            <p>Selain itu, kamu juga perlu melatih kejelasan dari tiap kata yang diucapkan. Hindari mengucapkan kata atau kalimat yang kurang jelas karena ini bisa mengesankan bahwa ada keraguan dalam diri kamu sebagai calon karyawan di tempat kerja mereka.</p>
+            
+            <p><strong>3. Hidupkan Suara dengan Teknis Periodik</strong></p>
+            
+            <p>Hindari suara yang monoton dan gunakan suara yang dinamis. Nada suara harus naik dan turun secara periodik. Penyiar radio adalah contoh yang bagus untuk urusan ini</p>
+            
+            <p>Hal ini tidak bisa kamu sepelekan karena jika diabaikan begitu saja maka sangat dimungkinkan kamu akan gugup saat menghadapi tahapan wawancara.</p>
+            
+            <p>Apalagi, jika sikap seperti ini kamu lakukan berkali-kali dalam sesi wawacara maka akan mengesankan bila kamu mengalami masalah dengan komunikasi.</p>
+            
+            <p>Ketiga langkah atau tips di atas merupakan cara secara teknis yang bisa kamu lakukan untuk melatih kemampuanmu dalam berkomunikasi. Sehingga, paling tidak akan membantumu lebih bisa mengikuti alur wawancara kerja.</p>
+            
+            <p>Selain itu, satu hal yang penting adalah selalu latih cara bicara kamu dengan nada yang periodik serta jelas pengucapan kata-katanya. Hal ini bertujuan agar pewawancara lebih mudah memahami jawaban kamu saat sedang dalam proses wawancara kerja.</p>',
+            'gambar' => 'komunikasi.jpg',
+        ]);
+        $article->save();
+
+        $article = Article::create([
+            'judul' =>
+                'Tips Bersikap Bijak Ketika Mendapatkan Kritis di Lingkungan Kerja',
+            'ringkasan' =>
+                'Bijaklah dalam menyikapi kritik karena itu dapat membawa perubahan kearah lebih baik.',
+            'content' => '<p>Manusia sebagai makluk sosial yang sering berjumpa dan berkomunikasi dengan orang lain pasti tidak lepas dari penilaian, pujian, bahkan kritikan. Tidak semua orang dapat menanggapi kritis dengan bijak. Respon dari setiap orang akan berbeda dengan orang lain. Tak kecuali di lingkungan kerja, kamu pasti akan dihadapkan dengan kritik. Terkadang sulit untuk kita berlapang dada ketika mendapatkan kritik. Sebagian orang menanggap kritis adalah hal yang menyakitkan dan menjauhkan apalagi ketika kritik tersebut dilakukan pada waktu yang tidak tepat dan intonasi yang kurang pas seperti dnegan cara membentak, nada tinggi dan dilakukan dibanyak orang.</p>
+
+            <h2>Tips Bersikap Bijak Ketika Mendapatkan Kritis di Lingkungan Kerja</h2>
+            
+            <p>Ketika seseorang mendapatkan kritik umumnya akan merasa tidak nyaman, tersinggung bahkan dapat membuat kehilangan kepercayaan diri. Situasi seperti ini memang tidak mudah, tetapi dibalik dengan kritik ada hal positif atau hikmah yang bisa dapatkan. Salah satunya dengan mendorong kamu untuk mengembangkan diri kamu kearah yang lebih baik. Dengan adanya kritik juga dapat menjadi motivasi untuk memperbaiki kesalahan kamu lalu tidak mengulangi kesalahan yang sama. Nah, supaya kamu dapat menyikapi kritik dengan bijak dan tidak membuat kinerjamu turun, ada beberapa cara yang bisa kamu lakukan, yakni:</p>
+            
+            <p><strong>1. Dengarkan kritik tersebut</strong></p>
+            
+            <p>Ketika kamu mendapatkan kritik dari seseorang, hal yang pertama kamu lakukan adalah dengarkan kritik tersebut baik-baik. Jangan lansgung membalas dengan argumenmu, apalagi disampaikan dengan emosi yang tidak stabil. Biarkan pemberi kritik menyampaikan kritiknya hingga selesai sambil kamu mengatur emosi. Jangan langsung berfikir negatif hal itu bisa membuat kamu menerima kritikan tersebut.</p>
+            
+            <p><strong>2. Pahami kritik yang disampaikan</strong></p>
+            
+            <p>Setelah selesai memberikan kritikan, kamu perlu untuk memahami kepada kamu dikritik, siapa yang memberi kritik, bagaimana cara penyampaikan kritik tersebut dan masih banyak lagi yang bisa kamu cari tahu. Ketika kamu hanya fokus kepada kritik dan emosimu tanpa memahami alasan di balik kritik tersebut maka kamu akan kesulitan dalam menerima dan&nbsp; berlapang dada.</p>
+            
+            <p><strong>3. Berterima kasih kepada pemberi kritik</strong></p>
+            
+            <p>Seseorang memberikan kritikan, artinya ada pesan yang ingin ia sampaikan berdasarkan apa yang ia lihat dari apa yang kamu lakukan. Diberikan kritikan kamu harus tetap rendah hati. Berikan ucapan terima kasih karena sudah mengingatkan dan memperhatikan apa yang kamu lakukan. Secara tidak langsung seseorang yang memberi kritikan artinya perduli kepadamu. Pada akhirnya kritik jika diterima dengan lapang dada akan membawa kamu pada perubahaan kearah lebih baik, tapi semua akan tergantung dengan bagaimana kamu menerima dan menyikapi kritikan tersebut.</p>
+            
+            <p><strong>4. Atur emosi</strong></p>
+            
+            <p>Penting untuk kita mengatur emosi ketika diberi kritikan. Ketika apa yang dikritik dan kamu terima merasa secara spontan pasti ingin membalas dengan argumenmu. Berusahaanlah untuk mengatur emosimu agar stabil sehingga kamu dapat berfikir secara rasional ketika dihadapkan dengan kritikan.</p>
+            
+            <p><strong>5. Ambil hikmah dari kritik tersebut</strong></p>
+            
+            <p>Setiap kritik yang disampaikan, kita bisa mengambil sisi positifnya. Terimalah pesan dengan lapang dada dan sikapi dengan pikiran yang jernih. Percayalah ketika kamu menyikapi kritik dengan bijak akan memberikan dampak positif atau hikmah dikemudian hari. Tidak hanya bermanfaat pada diri kamu sendiri tapi juga dengan pergaulan atau karirmu.</p>
+            
+            <p>Jangan langsung berkecil hati ketika kamu menerima kritikan. Seseorang memberikan kritik bukan berarti benci tetapi perduli kepadamu.</p>',
+            'gambar' => 'emosi.jpg',
         ]);
         $article->save();
     }
