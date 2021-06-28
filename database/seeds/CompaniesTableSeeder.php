6e162af907ea887a617b4688800961ca0867e3a6
ganisarya <madaniganis@gmail.com>
2021-06-28 09:55:14 +0700

    Seed Companies desk alamat

--- database/seeds/CompaniesTableSeeder.php
+++ database/seeds/CompaniesTableSeeder.php
@@ -15,7 +15,7 @@ class CompaniesTableSeeder extends Seeder
         $company = Company::create([
             'name'      => 'Areakerja',
             'deskripsi' => 'Areakerja merupakan perusahaan untuk mencari kerja',
-            'alamat'    => 'Jl. Kampar',
+            'alamat'    => 'Jl. Kanoman 23 Tegal Pasar ',
             'gambar'    => 'Areakerja.jpeg',
             'slug'      => 'areakerja',
         ]);
@@ -23,6 +23,8 @@ class CompaniesTableSeeder extends Seeder
 
         $company = Company::create([
             'name'   => 'Seven Inc',
+            'deskripsi' => 'Seven Inc merupakan perusahaan yang berfokus pada industri clothing/apparel pria',
+            'alamat'    => 'Jl. Janti gang Arjuna Banguntapan Bantul Yogyakarta',
             'gambar' => 'Seven Inc.png',
             'slug'   => 'seven-inc',
         ]);
@@ -30,6 +32,8 @@ class CompaniesTableSeeder extends Seeder
 
         $company = Company::create([
             'name'   => 'KopiSoe',
+            'deskripsi' => 'Kopi Soe merupakan Coffee Shop yang mengandalkan mengandalkan ciri khas jadul agar mendapat kesan Indonesia banget untuk penikmatnya',
+            'alamat'    => 'Jl. Colombo Yogyakarta',
             'gambar' => 'kopisoe.png',
             'slug'   => 'kopisoe',
         ]);
@@ -37,6 +41,8 @@ class CompaniesTableSeeder extends Seeder
 
         $company = Company::create([
             'name'   => 'Jogja Tailor',
+            'deskripsi' => 'Jogja Tailor melayani jasa jahitan yang eksklusif',
+            'alamat'    => 'Jl. Janti gang Arjuna Baguntapan',
             'gambar' => 'Jogja Tailor.png',
             'slug'   => 'jogja-tailor',
         ]);
@@ -44,6 +50,8 @@ class CompaniesTableSeeder extends Seeder
 
         $company = Company::create([
             'name'   => 'The Oxalis Regency Hotel',
+            'deskripsi' => 'The Oxalis Regency Hotel adalah tempat menginap ideal bagi para wisatawan yang mencari pesona dan kenyamanan dari kota Magelang',
+            'alamat'    => 'Jl. Cemp. No.17, Kemirirejo, Kec. Magelang Tengah, Kota Magelang, Jawa Tengah 56122',
             'gambar' => 'The Oxalis Regency Hotel.png',
             'slug'   => 'the-oxalis-regency-hotel',
         ]);
@@ -51,6 +59,8 @@ class CompaniesTableSeeder extends Seeder
 
         $company = Company::create([
             'name'   => 'JW Media Production',
+            'deskripsi' => 'JW Creative Production Adalah Pengelola Website dan Youtube',
+            'alamat'    => 'l. Cebongan No.310, Dukuh, Sidomoyo, Kec. Godean, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55264',
             'gambar' => 'JW Media Production.jpg',
             'slug'   => 'jw-media-production',
         ]);
@@ -58,6 +68,8 @@ class CompaniesTableSeeder extends Seeder
 
         $company = Company::create([
             'name'   => 'Dynamic Space',
+            'deskripsi' => 'Dynamic Space adalah perusahaan digutal marketing',
+            'alamat'    => 'Jl. Janti gang Bimo Baguntapan',
             'gambar' => 'Dynamic Space.png',
             'slug'   => 'dynamic-space',
         ]);
