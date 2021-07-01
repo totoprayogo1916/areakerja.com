de3e6a86afa18a73467f82401a323d80006901e9
ganisarya <madaniganis@gmail.com>
2021-07-01 11:03:17 +0700

    price seeder

--- database/seeds/PriceSeeder.php
+++ database/seeds/PriceSeeder.php
@@ -18,5 +18,26 @@ class PriceSeeder extends Seeder
             'warna'             => '#ffa70e',
         ]);
         $price->save();
+
+        $price = Price::create([
+            'nama'              => 'Silver',
+            'deskripsi_singkat' => 'Jangkauan Cukup Luas',
+            'deskripsi_full'    => '3 Kali Publikasi di semua jaringan AreaKerja.id',
+            'list'              => ['Website & Aplikasi', 'Instagram Post & Story', 'Google Jobs & Bisnis', 'Facebook Post & Story', 'Twitter', 'Linkedin', 'Telegram'],
+            'harga'             => 'Rp 5.000.000',
+            'warna'             => '#858990',
+        ]);
+        $price->save();
+
+        $price = Price::create([
+            'nama'              => 'Bronze',
+            'deskripsi_singkat' => 'Rekrut dengan Hemat',
+            'deskripsi_full'    => '1 Kali Publikasi di semua jaringan AreaKerja.id',
+            'list'              => ['Website & Aplikasi', 'Instagram Post & Story', 'Google Jobs & Bisnis', 'Facebook Post & Story', 'Twitter', 'Linkedin', 'Telegram'],
+            'harga'             => 'Rp 2.500.000',
+            'warna'             => '#605349',
+        ]);
+        $price->save();
+        
     }
 }
