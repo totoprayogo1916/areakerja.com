3b0554626c19de96ebeeacc183c15fcdbb4a4540
user <ti.ervany@gmail.com>
2021-07-22 10:44:24 +0700

    header mitra

--- resources/views/layouts/mitra/header.blade.php
+++ resources/views/layouts/mitra/header.blade.php
@@ -67,7 +67,7 @@
 
       <li class="dropdown"><a href="#" data-toggle="dropdown"
         {{-- {{ url('img/mitralogo') }}/{{ $mitra->logo }} --}}
-          class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="#"
+          class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="{{ url('img/mitralogo') }}/{{ $mitra->logo }}"
             class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
         <div class="dropdown-menu dropdown-menu-right pullDown">
           <div class="dropdown-title">Hello {{$mitra->nama}}</div>
