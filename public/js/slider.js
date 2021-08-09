99344acba8220e7527276b6f2cc9d34c91e0dc16
5h1ro <rajamodus7073@gmail.com>
2021-08-09 15:03:17 +0700

    ijo ijo benderane NU

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,17 +1,16 @@
+a = r;
 const script = document.createElement("script");
 script.type = "text/javascript";
-script.innerText = 'pacar' + r;
+script.innerText = 'console.log("costum-slider' + r + '");' +
+    a + '=' +
+    r + ';window["slider' +
+    a + '"] =document.getElementById("costum-slider' +
+    r + '").addEventListener("input",function(event) {window["value' +
+    a + '"] = event.target.value;document.getElementById("current-value' +
+    a + '").innerText = window["value' +
+    a + '"];});console.log("current-value' +
+    a + '");';
 document.body.appendChild(script);
 r++;
-// console.log("costum-slider" + r + "");
-// a = r;
-// window['slider' + a] =
-//     document.getElementById("costum-slider" + r + "").addEventListener("input",
-//         function(
-//             event) {
-//             let value = event.target.value;
-//             document.getElementById(
-//                 "current-value" + a + "").innerText = value;
-//         });
-// console.log("current-value" + a + "");
+
 // r++;
