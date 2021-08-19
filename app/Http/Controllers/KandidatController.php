64c41b8cb475cde2635b87f475c78acc3c9ea374
5h1ro <rajamodus7073@gmail.com>
2021-08-19 14:13:16 +0700

    form add to sementara, and acc

--- app/Http/Controllers/KandidatController.php
+++ app/Http/Controllers/KandidatController.php
@@ -3,39 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Calonkandidat;
+use App\Organisasi;
+use App\Pengalaman;
 use Illuminate\Http\Request;
 use App\RiwayatPendidikan;
+use App\Skill;
 
 class KandidatController extends Controller
 {
     public function formkandidat(Request $request)
     {
-        $jumlah_school = count(collect($request->school));
+        Calonkandidat::create([
+            'namalengkap' => $request->namalengkap,
+            'alamat'      => $request->alamat,
+            'biografi'    => $request->biografi,
+            'skillutama'  => $request->skillutama,
+            'namaskill'   => $request->namaskill,
+            'rangeskill'  => $request->rangeskill,
+            'school'      => $request->school,
+            'tahun'       => $request->tahun,
+            'organisasi'  => $request->organisasi,
+            'jabatan'     => $request->jabatan,
+            'kantor'      => $request->kantor,
+            'pekerjaan'   => $request->pekerjaan,
+        ]);
+        return redirect()->route('home');
+    }
+
+    public function acc(){
+        $calon = Calonkandidat::first();
+        $jumlah_school = count(collect($calon->school));
 
         for ($i = 0; $i < $jumlah_school; $i++){
              RiwayatPendidikan::create([
-                'nama_instansi' => $request->school[$i],
-                'tahun' => $request->tahun[$i]
+                'nama_instansi' => $calon->school[$i],
+                'tahun' => $calon->tahun[$i]
             ]);
         }
 
-        // foreach ($request->school as $school) {
-            // dd($school);
+        $jumlah_skill = count(collect($calon->namaskill));
 
-    //     Calonkandidat::create([
-    //         'namalengkap' => $request->namalengkap,
-    //         'alamat'      => $request->alamat,
-    //         'biografi'    => $request->biografi,
-    //         'skillutama'  => $request->skillutama,
-    //         'namaskill'   => $request->namaskill,
-    //         'rangeskill'  => $request->rangeskill,
-    //         'school'      => $request->school,
-    //         'tahun'       => $request->tahun,
-    //         'organisasi'  => $request->organisasi,
-    //         'jabatan'     => $request->jabatan,
-    //         'kantor'      => $request->kantor,
-    //         'pekerjaan'   => $request->pekerjaan,
-    //     ]);
-        // }
+        for ($i = 0; $i < $jumlah_skill; $i++){
+             Skill::create([
+                'nama' => $calon->namaskill[$i],
+                'tingkat' => $calon->rangeskill[$i]
+            ]);
+        }
+
+        $jumlah_organisasi = count(collect($calon->organisasi));
+
+        for ($i = 0; $i < $jumlah_organisasi; $i++){
+             Organisasi::create([
+                'judul' => $calon->organisasi[$i],
+                'jabatan' => $calon->jabatan[$i]
+            ]);
+        }
+
+        $jumlah_pengalaman = count(collect($calon->kantor));
+
+        for ($i = 0; $i < $jumlah_pengalaman; $i++){
+             Pengalaman::create([
+                'nama_pengalaman' => $calon->kantor[$i],
+                'posisi' => $calon->pekerjaan[$i]
+            ]);
+        }
     }
 }
