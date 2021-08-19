3fd715f3440deea2182eee2d7f5740f7f628272d
5h1ro <rajamodus7073@gmail.com>
2021-08-19 14:01:35 +0700

    kandidat controller

--- app/Http/Controllers/KandidatController.php
+++ app/Http/Controllers/KandidatController.php
@@ -4,25 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Calonkandidat;
 use Illuminate\Http\Request;
+use App\RiwayatPendidikan;
 
 class KandidatController extends Controller
 {
     public function formkandidat(Request $request)
     {
-        // dd($request);
-        Calonkandidat::create([
-            'namalengkap' => $request->namalengkap,
-            'alamat'      => $request->alamat,
-            'biografi'    => $request->biografi,
-            'skillutama'  => $request->skillutama,
-            'namaskill'   => $request->namaskill,
-            'rangeskill'  => $request->rangeskill,
-            'school'      => $request->school,
-            'tahun'       => $request->tahun,
-            'organisasi'  => $request->organisasi,
-            'jabatan'     => $request->jabatan,
-            'kantor'      => $request->kantor,
-            'pekerjaan'   => $request->pekerjaan,
-        ]);
+        $jumlah_school = count(collect($request->school));
+
+        for ($i = 0; $i < $jumlah_school; $i++){
+             RiwayatPendidikan::create([
+                'nama_instansi' => $request->school[$i],
+                'tahun' => $request->tahun[$i]
+            ]);
+        }
+
+        // foreach ($request->school as $school) {
+            // dd($school);
+
+    //     Calonkandidat::create([
+    //         'namalengkap' => $request->namalengkap,
+    //         'alamat'      => $request->alamat,
+    //         'biografi'    => $request->biografi,
+    //         'skillutama'  => $request->skillutama,
+    //         'namaskill'   => $request->namaskill,
+    //         'rangeskill'  => $request->rangeskill,
+    //         'school'      => $request->school,
+    //         'tahun'       => $request->tahun,
+    //         'organisasi'  => $request->organisasi,
+    //         'jabatan'     => $request->jabatan,
+    //         'kantor'      => $request->kantor,
+    //         'pekerjaan'   => $request->pekerjaan,
+    //     ]);
+        // }
     }
 }
