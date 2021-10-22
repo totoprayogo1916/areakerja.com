c34d714399dfb2da921d69abf53afd3cc3f3975b
user <ti.ervany@gmail.com>
2021-10-22 16:07:34 +0700

    scroll display

--- resources/views/layouts/pasang.blade.php
+++ resources/views/layouts/pasang.blade.php
@@ -186,10 +186,10 @@
         mybutton.style.display = "block";
 
         function scrollFunction() {
-            if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-                mybutton.style.display = "none";
-            } else {
+            if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
                 mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
             }
         }
 
