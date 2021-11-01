c1a2f2166c3543662feadb70ededa8cbb2803597
5h1ro <rajamodus7073@gmail.com>
2021-11-02 02:49:23 +0700

    sala wkwkwk

--- app/Http/Controllers/Mitra/DaftarLowonganController.php
+++ app/Http/Controllers/Mitra/DaftarLowonganController.php
@@ -59,6 +59,6 @@ class DaftarLowonganController extends Controller
         $mitra->koin = $mitra->koin - 2;
         $mitra->save();
 
-        return route('mitra.lowongan.index');
+        return redirect()->route('mitra.lowongan.index');
     }
 }
